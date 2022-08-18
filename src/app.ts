ffaa4937e6a085dd2b52ff2520824224a10341f7
Gabriel <kastro.gabriel17@gmail.com>
2022-08-17 21:46:45 -0400

    feat: middleware error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import cors from "cors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import * as Yup from "yup";
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  if (err instanceof Yup.ValidationError) {
+    return res.status(400).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+});
 
 app.get("/", (_, res) => {
   res.json({ ok: true });
