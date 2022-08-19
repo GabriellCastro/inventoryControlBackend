0ddc44b30553d1fa1162741bf637007be0795946
Gabriel <kastro.gabriel17@gmail.com>
2022-08-19 16:35:07 -0400

    fix: add router

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import * as Yup from "yup";
 import { AppError } from "./errors/AppError";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
